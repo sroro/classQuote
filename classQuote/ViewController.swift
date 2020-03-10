af04192341394ebc7d3d29f27f9e4babf385ce0b
sroro <rodolphe.schnetzer@gmail.com>
2020-03-10 16:50:45 +0100

    Initial Commit

---
+++ classQuote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  classQuote
+//
+//  Created by Rodolphe Schnetzer on 10/03/2020.
+//  Copyright © 2020 Rodolphe Schnetzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
